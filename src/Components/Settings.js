9ea339f3a69d496b7a95495e9a67ff059d8d6060
github username <github email address>
2019-05-23 19:30:25 -0400

    next commit

---
+++ src/Components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Settings extends React.Component {
+
+    render(){
+      return (
+       <h1>"I'm the settings page!"</h1>
+      )
+    }
+  }
+
+  export default Settings;
---
+++ src/Containers/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Main extends React.Component {
+
+    render(){
+      return (
+       <h1>"I'm the Main Page!"</h1>
+      )
+    }
+  }
+
+  export default Main;
