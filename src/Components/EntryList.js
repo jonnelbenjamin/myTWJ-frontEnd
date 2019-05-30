6979336c7083d5591f06e990f6bb223487e91d40
github username <github email address>
2019-05-30 11:05:29 -0500

    delete function works

--- src/Components/EntryList.js
+++ src/Components/EntryList.js
@@ -5,87 +5,98 @@ import ReactSearchBox from 'react-search-box'
 
 
 class EntryList extends React.Component {
-    constructor(){
+  constructor() {
     super()
     this.state = {
-        entryList: [],
-        date: "",
-        search: ""
+      entryList: [],
+      date: "",
+      search: ""
     }
-}
+  }
 
-componentDidMount(){
+  componentDidMount() {
     fetch('http://localhost:3000/entries')
-    .then(res => res.json())
-    .then(entryList => this.setState({
+      .then(res => res.json())
+      .then(entryList => this.setState({
         entryList
-    }))
-}
- 
+      }))
+  }
 
-onCalendarChange = (date) => {
-  let newDate = this.formatDateVariable(date)
-  this.setState({ date:newDate, search: "" })}
 
-searchChange = (e) => {
+  onCalendarChange = (date) => {
+    let newDate = this.formatDateVariable(date)
+    this.setState({ date: newDate, search: "" })
+  }
+
+  searchChange = (e) => {
     this.setState({
-    search: e.target.value,
-    date: ""
-})
-}
-
-formatDateVariable = (dateVariable) => {
-  let day = dateVariable.getDate()
-  let month = dateVariable.getMonth() + 1
-  let year = dateVariable.getFullYear()
-  // 2019-05-29
-  
-  if(month >= 10 && day < 10){
-    let parsedString = `${month}-0${day}-${year}`
-    return parsedString
+      search: e.target.value,
+      date: ""
+    })
   }
-  else if(month < 10 && day < 10){
-  let parsedString = `${month}-0${day}-${year}`
-    return parsedString 
-}
-  else if(day >= 10 && month <10 ){
-    let parsedString = `${month}-${day}-${year}`
-    return parsedString
+
+  formatDateVariable = (dateVariable) => {
+    let day = dateVariable.getDate()
+    let month = dateVariable.getMonth() + 1
+    let year = dateVariable.getFullYear()
+    // 2019-05-29
+
+    if (month >= 10 && day < 10) {
+      let parsedString = `${month}-0${day}-${year}`
+      return parsedString
+    }
+    else if (month < 10 && day < 10) {
+      let parsedString = `${month}-0${day}-${year}`
+      return parsedString
+    }
+    else if (day >= 10 && month < 10) {
+      let parsedString = `${month}-${day}-${year}`
+      return parsedString
+    }
+    else {
+      let parsedString = `${month}-${day}-${year}`
+      return parsedString
+    }
+
   }
-  else {
-    let parsedString = `${month}-${day}-${year}`
-    return parsedString
+
+  handleDelete = (e) => {
+    fetch(`http://localhost:3000/entries/${e.target.id}`, {
+      method: "DELETE"
+    }
+
+    )
   }
-  
-}
 
 
-    render(){
-      return (
-        <div>
-       
-      <Table id="journalEntries">
-      <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell colSpan='3'>Entries List</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-          <Table.Body>
-           
-        {this.state.search ? this.state.entryList.map(entry => entry.description.includes(this.state.search) ?  <Table.Row>
-            <Table.Cell>{entry.description}</Table.Cell>
-            <Table.Cell>{entry.date_and_time}</Table.Cell>
-            </Table.Row> : null) : this.state.date ? this.state.entryList.map(entry => entry.date_and_time.startsWith(this.state.date) 
-            ? <Table.Row><Table.Cell>{entry.description}</Table.Cell>
-            <Table.Cell>{entry.date_and_time}</Table.Cell>
-            </Table.Row> : null)
-             : this.state.entryList.map(entry => 
+  render() {
+    return (
+      <div>
+
+        <Table id="journalEntries">
+          <Table.Header>
             <Table.Row>
-            <Table.Cell>{entry.description}</Table.Cell>
-            <Table.Cell>{entry.date_and_time}</Table.Cell>
+              <Table.HeaderCell colSpan='3'>Entries List</Table.HeaderCell>
             </Table.Row>
-            )}
-            </Table.Body>
+          </Table.Header>
+          <Table.Body>
+
+            {this.state.search ? this.state.entryList.map(entry => entry.description.includes(this.state.search) ? <Table.Row>
+              <Table.Cell>{entry.description}</Table.Cell>
+              <Table.Cell>{entry.date_and_time}</Table.Cell>
+            </Table.Row> : null) 
+            : this.state.date ? 
+            this.state.entryList.map(entry => entry.date_and_time.startsWith(this.state.date)
+              ? <Table.Row><Table.Cell>{entry.description}</Table.Cell>
+                <Table.Cell>{entry.date_and_time}</Table.Cell>
+              </Table.Row> : null)
+                : this.state.entryList.map(entry =>
+                  <Table.Row>
+                    <Table.Cell>{entry.description}</Table.Cell>
+                    <Table.Cell>{entry.date_and_time}</Table.Cell>
+                    <Table.Cell><Icon name='trash' onClick={this.handleDelete} id={entry.id}/></Table.Cell>
+                  </Table.Row>)}
+          </Table.Body>
         </Table>
         <Calendar
           onChange={this.onCalendarChange}
@@ -94,10 +105,10 @@ formatDateVariable = (dateVariable) => {
         <div id='search'>
           <Search onSearchChange={this.searchChange}
           ></Search>
-        </div> 
-       </div>
-      )
-    }
+        </div>
+      </div>
+    )
   }
+}
 
-  export default EntryList;
+export default EntryList;
