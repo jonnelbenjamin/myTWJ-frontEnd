e327f4c27ce2f69f375b21a68df8b6c7a3e84047
github username <github email address>
2019-06-03 21:08:43 -0400

    auth

--- src/App.js
+++ src/App.js
@@ -4,11 +4,74 @@ import Navbar from './Containers/Navbar.js'
 import Main from './Containers/Main.js'
 
 function App() {
+  
+  state = {
+    currentUser: null,
+    loading: true
+  }
+  
+ componentDidMount();{
+
+   this.mount()
+ }
+ 
+
+  mount = () => {
+    let token = localStorage.getItem('token')
+    if(token){
+      fetch(`http://localhost:3000/profile`, {
+        method: "GET",
+        headers: {
+          "Authentication": `Bearer ${token}`}
+          })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          currentUser: data,
+          loading:false})
+      })
+    }else{
+      this.setState({loading: false})
+    }
+  }
+
+  handleLoginSubmit = (username, password) => {
+    fetch(`http://localhost:3000/login`, {
+      method: "POST",
+        headers: {
+          "Content-Type":"application/json",
+          "Accept":"application/json"
+        },
+        body: JSON.stringify({
+          username:username,
+          password:password
+        })
+    })
+    .then(res =>res.json())
+    .then(data => {
+      if(data.error){
+        console.log(data)
+        alert('Incorrect username or password')
+      }else{
+        console.log(data)
+        this.setState({currentUser: data.user })
+        localStorage.setItem("token", data.token)
+      }
+    })
+  }
+
+  handleLogOut = () => {
+    this.setState({
+      currentUser: null
+    })
+    localStorage.clear()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar/>
-        <Main/>
+        <Navbar logged_in={this.state.currentUser} onLogOut={this.handleLogOut}/>
+        <Main />
         
       </header>
     </div>
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,15 +2,45 @@ import React from 'react'
 
 class Login extends React.Component {
 
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+//
+  handleLogin = () => {
+    let userObj = this.state
+  fetch(`http://localhost:3000/login`, {
+       method:"POST",
+       headers: {
+         "Content-type":"application/json",
+       },
+       body: JSON.stringify({user: userObj})
+       }).then(res => res.json())
+        .then(data => {
+          console.log(data)
+          if(data.error){
+            alert('Incorrect email and/or password')
+          } else {
+            console.log('Login Successful')
+            localStorage.setItem('token', data.token)
+          }
+       }
+     )
+      }
+
     render(){
       return (
         <div id="login">
        <h1>"I'm the login page!"</h1>
        <form>
-       <input placeholder='email' type='email'></input>
-       <input placeholder='password' type='password'></input>
+       <input name='email' placeholder='email' type='email' onChange={this.handleChange}></input>
+       <input name='password' placeholder='password' type='password' onChange={this.handleChange}></input>
        <br></br>
-       <button>Login</button>
+       <button onClick={this.handleLogin}>Login</button>
        </form>
 
        </div>
